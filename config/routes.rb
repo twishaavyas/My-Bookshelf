1dec5f1872154afd63f723d4cda40d21b8cebe5c
Twisha <twisha.vyas@mirraw.com>
2016-08-12 16:29:06 +0530

    Authenticating Users done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  
+
+ 	get 'admin' => 'admin#index'
+	controller :sessions do
+		get 'login' => :new
+		post 'login' => :create
+		delete 'logout' => :destroy
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   	get "store/index"
 
