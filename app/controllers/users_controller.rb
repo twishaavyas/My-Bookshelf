c3a5d6269cdb84814f5c980d273ac52f67358f97
Twisha <twisha.vyas@mirraw.com>
2016-08-12 16:04:41 +0530

    user created

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,65 @@
+class UsersController < ApplicationController
+	protect_from_forgery with: :exception
+	
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def index
+		@users = User.order(:name)
+	end
+
+	def new
+		@user = User.new
+	end
+
+	# def create
+	# 	@user = User.new(user_params)
+	# 	if @user.save
+	# 		redirect_to @user
+	# 	else
+	# 		render 'new'
+	# 	end
+	# end
+
+	def create
+		@user = User.new(user_params)
+		respond_to do |format|
+			if @user.save
+				format.html { redirect_to(users_url,
+				:notice => "User #{@user.name} was successfully created.") }
+				format.xml { render :xml => @user,
+				:status => :created, :location => @user }
+			else
+				format.html { render :action => "new" }
+				format.xml { render :xml => @user.errors,
+				:status => :unprocessable_entity }
+			end
+		end
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		redirect_to users_path
+	end
+
+	private
+		def user_params
+			params.require(:user).permit(:name ,:password,:password_confirmation)
+		end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 	resources :products
 	resources :carts
 	resources :line_items
+	resources :users
 	
 	
 end
